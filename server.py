7cc46f3aff529ca5b6592ec526f79dc6bf4f7e12
Christian Ramirez <christian.r0602@gmail.com>
2022-03-30 11:54:52 -0500

    template engines and bootstrap

--- server.py
+++ server.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/lists')
+def render():
+    users = [
+   {'first_name' : 'Michael', 'last_name' : 'Choi'},
+   {'first_name' : 'John', 'last_name' : 'Supsupin'},
+   {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+   {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+
+    return render_template('index.html', names = users)
 
 
 
